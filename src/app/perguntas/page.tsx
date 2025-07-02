0a5dec4ea1469954f0dd9f61c6c58d46d17ed433
Fupicat <fupicat@gmail.com>
2025-07-01 22:08:44 -0300

    filtros

--- src/app/perguntas/page.tsx
+++ src/app/perguntas/page.tsx
@@ -6,7 +6,76 @@ export default function Perguntas() {
     <>
       <Header path="/perguntas" />
       <main>
-        <h1>Perguntas</h1>
+        <div>
+          <h2>Perguntas e Respostas</h2>
+        </div>
+        <aside>
+          <h3>Filtros</h3>
+          <form className="filtros">
+            <div className="busca">
+              <input type="search" name="busca" placeholder="Buscar..." />
+              <button type="submit">Buscar</button>
+            </div>
+            <div className="select">
+              <label htmlFor="curso">Curso</label>
+              <select name="curso" id="curso">
+                <option value="">Todos os cursos</option>
+                <option value="ciencia-da-computacao">
+                  Ciência da Computação
+                </option>
+                <option value="analise-e-desenvolvimento-de-sistemas">
+                  Análise e Desenvolvimento de Sistemas
+                </option>
+              </select>
+            </div>
+            <div className="select">
+              <label htmlFor="materia">Matéria</label>
+              <select name="materia" id="materia">
+                <option value="">Todas as matérias</option>
+                <option value="programacao">Programação</option>
+                <option value="banco-de-dados">Banco de Dados</option>
+                <option value="redes">Redes</option>
+              </select>
+            </div>
+            <div className="checkboxes">
+              <h4>Incluir:</h4>
+              <div>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="resolvidas"
+                    defaultChecked={true}
+                  />
+                  Resolvidas
+                </label>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="nao-resolvidas"
+                    defaultChecked={true}
+                  />
+                  Não resolvidas
+                </label>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="favoritas"
+                    defaultChecked={true}
+                  />
+                  Favoritas
+                </label>
+                <label>
+                  <input type="checkbox" name="minhas" />
+                  Minhas perguntas
+                </label>
+                <label>
+                  <input type="checkbox" name="minhas-respostas" />
+                  Minhas respostas
+                </label>
+              </div>
+            </div>
+          </form>
+        </aside>
       </main>
     </>
   );
